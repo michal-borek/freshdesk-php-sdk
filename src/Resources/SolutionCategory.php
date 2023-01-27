9d60c9d2ff44382c4189d224597efe0bf64838fb
arjan <arjande.ruyter@buroscanbrit.nl>
2023-01-27 14:39:26 +0100

    Solutions setup

---
+++ src/Resources/SolutionCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Freshdesk\Resources;
+use Freshdesk\Resources\Traits\AllTrait;
+use Freshdesk\Resources\Traits\CreateTrait;
+use Freshdesk\Resources\Traits\DeleteTrait;
+use Freshdesk\Resources\Traits\UpdateTrait;
+use Freshdesk\Resources\Traits\ViewTrait;
+
+class SolutionCategory extends AbstractResource
+{
+
+    use AllTrait, CreateTrait, ViewTrait, UpdateTrait, DeleteTrait;
+
+    /**
+     * The resource endpoint
+     *
+     * @internal
+     * @var string
+     *
+     */
+    protected $endpoint = '/solutions/categories';
+
+    public function create(array $data)
+    {
+        return $this->api()->request('POST', $this->endpoint, $data);
+    }
+
+
+}
---
+++ src/Resources/SolutionCategoryFolder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Freshdesk\Resources;
+use Freshdesk\Resources\Traits\AllTrait;
+use Freshdesk\Resources\Traits\CreateTrait;
+use Freshdesk\Resources\Traits\DeleteTrait;
+use Freshdesk\Resources\Traits\UpdateTrait;
+use Freshdesk\Resources\Traits\ViewTrait;
+
+class SolutionCategoryFolder extends AbstractResource
+{
+
+    use AllTrait, CreateTrait, ViewTrait, UpdateTrait, DeleteTrait;
+
+    /**
+     * The resource endpoint
+     *
+     * @internal
+     * @var string
+     *
+     */
+
+    private $categoriesEndpoint = '/solutions/categories';
+    private $foldersEndpoint = '/solutions/folders';
+
+      /**
+     * Creates the categories endpoint
+     *
+     * @param string $id
+     * @return string
+     * @internal
+     */
+    protected function categoriesEndpoint($id = null)
+    {
+        return $id === null ? $this->categoriesEndpoint : $this->categoriesEndpoint . '/' . $id;
+    }
+
+        /**
+     * Creates the folders endpoint
+     *
+     * @param string $id
+     * @return string
+     * @internal
+     */
+    protected function foldersEndpoint($id = null)
+    {
+        return $id === null ? $this->foldersEndpoint : $this->foldersEndpoint . '/' . $id;
+    }
+
+
+    /**
+     *
+     * List folders in categories
+     *
+     * @api
+     * @param int $id
+     * @param array|null $query
+     * @return array|null
+     * @throws \Freshdesk\Exceptions\AccessDeniedException
+     * @throws \Freshdesk\Exceptions\ApiException
+     * @throws \Freshdesk\Exceptions\AuthenticationException
+     * @throws \Freshdesk\Exceptions\ConflictingStateException
+     * @throws \Freshdesk\Exceptions\NotFoundException
+     * @throws \Freshdesk\Exceptions\RateLimitExceededException
+     * @throws \Freshdesk\Exceptions\UnsupportedContentTypeException
+     * @throws \Freshdesk\Exceptions\MethodNotAllowedException
+     * @throws \Freshdesk\Exceptions\UnsupportedAcceptHeaderException
+     * @throws \Freshdesk\Exceptions\ValidationException
+     */
+    public function all($id, array $query = null)
+    {
+        return $this->api()->request('GET', $this->categoriesEndpoint($id . '/folders'), null, $query);
+    }
+
+    public function create($id, array $data)
+    {
+        return $this->api()->request('POST', $this->foldersEndpoint($id . '/folders'), $data);
+    }
+
+
+}
